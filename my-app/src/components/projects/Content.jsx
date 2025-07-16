06f1b0c17cbe65cce9cc4aba095deffd36d41cb0
Julia Fu <wanjia_fu@brown.edu>
2025-07-16 18:06:33 -0400

    proj

--- my-app/src/components/projects/Content.jsx
+++ my-app/src/components/projects/Content.jsx
@@ -4,10 +4,66 @@ import GraphicsImg from "../../assets/graphics.png";
 import DlImg from "../../assets/dl.png";
 import CvImg from "../../assets/cv.png";
 import SweImg from "../../assets/swe.png";
+import ResearchImg from "../../assets/research.png";
+import IRLImg from "../../assets/irl.jpg";
 
 const Content = () => {
     return (
         <div className="projects__info grid">
+            <div className="projects__box">
+                <div className="projects__topic">
+                    <h3 className="projects__title">Vibration Haptics: Hand contact detection and localization with IMU</h3>
+                    <span className="projects__date">Jan 2024 - Sep 2024</span>
+                </div>
+                <a className="projects__subtitle" href="https://ivl.cs.brown.edu/"
+                    target="_blank" rel="noreferrer">Brown Interactive 3D Vision & Learning Lab</a>
+                <div>
+                    <span className="projects__subheading">Advisors: Srinath Sridhar, Krishna Murthy Jatavallabhula</span>
+                </div>
+                <div className="projects__description">
+                    <li>
+                        {/* Training a model to detect hand-object contact and to localize the contact onto fingers with acceleration and gyroscope data from IMU sensors.  */}
+                        Designed portable wrist hardware device containing with Inertial Measurement Unit (IMU), using which to collect hand object contact sensor and visual data
+                    </li>
+                    <li>
+                        {/* Developed and 3D printed brace watch-like model to incorporate IMU and collect data. */}
+                        Preprocessed RGB camera data with MANO fitting pipeline to obtain pose estimation results and contact heatmap, 
+                        built a neural network to improve hand-object contact detection and localization 
+                    </li>
+                    <li>
+                        {/* Collected data using Brown Interactive Camera System (BRICS) and labeled data by incorporating the MANO fitting pipeline.  */}
+                        Integrated IMU sensor and its data visualization into Brown Interaction Capture System (BRICS), participated in the hardware assembling and software calibration for the room-sized capture studio with professor Dr. Srinath Sridhar
+                    </li>
+                </div>
+                <img src={ResearchImg} alt="" className="projects__img" />
+            </div>
+            <div className="projects__box">
+                <div className="projects__topic">
+                    <h3 className="projects__title">Shaped-Based Skill Transfer by Learning Policy on Object Parts</h3>
+                    <span className="projects__date" id="irl">2023-2024</span>
+                </div>
+                <a className="projects__subtitle" href="http://irl.cs.brown.edu/"
+                    target="_blank" rel="noreferrer">Brown Intelligent Robotics Lab</a>
+                <div>
+                    <span className="projects__subheading">Advisor: George Konidaris</span>
+                </div>
+                <div className="projects__description">
+                    <li>
+                        Learned latent representations of object parts for mugs and spatulas to carry out pouring and scooping tasks, and tried to learn a 
+                        robust skill for all shapes of the same object category, with advisor Dr. George Konidaris
+                    </li>
+                    <li>
+                        Incorporated Segment-Anything model to segment images of mugs into cup and handle and back projected the segmentation into point clouds. 
+                    </li>
+                    <li>
+                        Developed a policy with lower costs than when learning on the whole object instead of object parts
+                    </li>
+                    <li>
+                        Worked on ROS and reinforcement learning, Boston Dynamics Spot robots, and KUKA robotic arms with radar cameras
+                    </li>
+                </div>
+                <img src={IRLImg} alt="" className="experience__img" />
+            </div>
             <div className="projects__box">
                 <div className="projects__topic">
                     <h3 className="projects__title">MODEL BASED REINFORCEMENT LEARNING</h3>
