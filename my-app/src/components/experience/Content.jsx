0f629945d886a3f7dc73845995adcb7ad12a4ce9
Julia Fu <wanjia_fu@brown.edu>
2025-07-16 17:59:31 -0400

    overhaul

--- my-app/src/components/experience/Content.jsx
+++ my-app/src/components/experience/Content.jsx
@@ -1,27 +1,67 @@
 import React from 'react';
 import TutorialImg from "../../assets/tutorial.png";
+import ResearchImg from "../../assets/research.png";
+import IRLImg from "../../assets/irl.jpg";
 
 const Content = () => {
     return (
         <div className="experience__info grid">
             <div className="experience__box">
                 <div className="experience__topic">
-                    <h3 className="experience__title"> Intelligent Customer Service Product Operation Engineer Intern</h3>
-                    <span className="experience__date">May - June 2023</span>
+                    <h3 className="experience__title">Vibration Haptics: Hand contact detection and localization with IMU</h3>
+                    <span className="experience__date">Jan 2024 - Sep 2024</span>
                 </div>
-                <a className="experience__subtitle" href="https://www.hydsoft.com/en/"
-                target="_blank" rel="noreferrer">Hydsoft Technology Co., Ltd.</a>
+                <a className="experience__subtitle" href="https://ivl.cs.brown.edu/"
+                    target="_blank" rel="noreferrer">Brown Interactive 3D Vision & Learning Lab</a>
                 <div>
-                    <span className="experience__subheading">Software Technology Outsourcing Services Company</span>
+                    <span className="experience__subheading">Advisors: Srinath Sridhar, Krishna Murthy Jatavallabhula</span>
                 </div>
                 <div className="experience__description">
-                    <li>Designed AI customer service to improve plane ticket booking system based on Baidu UNIT and natural language processing                    </li>
-                    <li>Received the PaddlePaddle AI Technical Engineer Certificate</li>
-                    <li>Received the Certificate of Achievement as Intelligent Customer Service Product Operation Engineer by Baidu AI Cloud</li>
-                    <li>Designed front-end poster layout and user interface in a 10-member team for China Mobile Smart Card Production Platform                    </li>
+                    <li>
+                        {/* Training a model to detect hand-object contact and to localize the contact onto fingers with acceleration and gyroscope data from IMU sensors.  */}
+                        Designed portable wrist hardware device containing with Inertial Measurement Unit (IMU), using which to collect hand object contact sensor and visual data
+                    </li>
+                    <li>
+                        {/* Developed and 3D printed brace watch-like model to incorporate IMU and collect data. */}
+                        Preprocessed RGB camera data with MANO fitting pipeline to obtain pose estimation results and contact heatmap, 
+                        built a neural network to improve hand-object contact detection and localization 
+                    </li>
+                    <li>
+                        {/* Collected data using Brown Interactive Camera System (BRICS) and labeled data by incorporating the MANO fitting pipeline.  */}
+                        Integrated IMU sensor and its data visualization into Brown Interaction Capture System (BRICS), participated in the hardware assembling and software calibration for the room-sized capture studio with professor Dr. Srinath Sridhar
+                    </li>
                 </div>
+                <img src={ResearchImg} alt="" className="experience__img" />
             </div>
             <div className="experience__box">
+                <div className="experienceh__topic">
+                    <h3 className="experience__title">Shaped-Based Skill Transfer by Learning Policy on Object Parts</h3>
+                    <span className="experience__date" id="irl">2023-2024</span>
+                </div>
+                <a className="experience__subtitle" href="http://irl.cs.brown.edu/"
+                target="_blank" rel="noreferrer">Brown Intelligent Robotics Lab</a>
+                <div>
+                    <span className="experience__subheading">Advisor: George Konidaris</span>
+                </div>
+                <div className="experience__description">
+                    <li>
+                        Learned latent representations of object parts for mugs and spatulas to carry out pouring and scooping tasks, and tried to learn a 
+                        robust skill for all shapes of the same object category, with advisor Dr. George Konidaris
+                    </li>
+                    <li>
+                        Incorporated Segment-Anything model to segment images of mugs into cup and handle and back projected the segmentation into point clouds. 
+                    </li>
+                    <li>
+                        Developed a policy with lower costs than when learning on the whole object instead of object parts
+                    </li>
+                    <li>
+                        Worked on ROS and reinforcement learning, Boston Dynamics Spot robots, and KUKA robotic arms with radar cameras
+                    </li>
+                                    
+                </div>
+                <img src={IRLImg} alt="" className="experience__img" />
+            </div>
+                       <div className="experience__box">
                 <div className="experience__topic">
                     <h3 className="experience__title"> Software Engineering Intern at Brown Visual Computing</h3>
                     <div className="experience__social">
@@ -76,6 +116,25 @@ const Content = () => {
                     develop the course with professor Dr. Srinath Sridhar, and host weekly grading/staff meetings</li>
                 </div>
             </div>
+            
+            <div className="experience__box">
+                <div className="experience__topic">
+                    <h3 className="experience__title"> Intelligent Customer Service Product Operation Engineer Intern</h3>
+                    <span className="experience__date">May - June 2023</span>
+                </div>
+                <a className="experience__subtitle" href="https://www.hydsoft.com/en/"
+                target="_blank" rel="noreferrer">Hydsoft Technology Co., Ltd.</a>
+                <div>
+                    <span className="experience__subheading">Software Technology Outsourcing Services Company</span>
+                </div>
+                <div className="experience__description">
+                    <li>Designed AI customer service to improve plane ticket booking system based on Baidu UNIT and natural language processing                    </li>
+                    <li>Received the PaddlePaddle AI Technical Engineer Certificate</li>
+                    <li>Received the Certificate of Achievement as Intelligent Customer Service Product Operation Engineer by Baidu AI Cloud</li>
+                    <li>Designed front-end poster layout and user interface in a 10-member team for China Mobile Smart Card Production Platform                    </li>
+                </div>
+            </div>
+       
         </div>
     )
 }
--- my-app/src/components/research/Info.jsx
+++ my-app/src/components/research/Info.jsx
@@ -1,132 +1,91 @@
 import React from 'react'
-import ResearchImg from "../../assets/research.png";
-import IRLImg from "../../assets/irl.jpg";
-import HandsImg from "../../assets/GigaHands.png";
-import UniTacImg from "../../assets/tactile-teaser.jpg";
+import HandsImg from "../../assets/gigahands.gif";
+import UniTacImg from "../../assets/UniTac.gif";
 
 const Info = () => {
     return (
         <div className="research__info grid">
             <div className="research__box">
-                <div className="research__topic">
-                    <a className="research__title" href="https://ivl.cs.brown.edu/research/unitac"> UniTac: Whole-Robot Touch Sensing Without Tactile Sensors</a>
-                    <span className="research__date"> Nov 2024 - Mar 2025</span>
-                    <div className="research__social">
-                        <a href="https://arxiv.org/abs/2507.07980" 
-                        className="research__social-icon" rel="noreferrer" target="_blank">
-                        <i class="uil uil-file-alt"></i>
-                        </a>
+                <div className="research__teaser">
+                    <img src={UniTacImg} alt="" className="research__img" />
+                </div>
+                <div className="research__paragraph">
+                    <div className="research__topic">
+                        <a className="research__title" href="https://ivl.cs.brown.edu/research/unitac"> UniTac: Whole-Robot Touch Sensing Without Tactile Sensors</a>
+                        {/* <div className="research__social">
+                            <a href="https://arxiv.org/abs/2507.07980" 
+                            className="research__social-icon" rel="noreferrer" target="_blank">
+                            <i class="uil uil-file-alt"></i>
+                            </a>
+                        </div> */}
                     </div>
-                </div>
-                <a className="research__subtitle" href="https://ivl.cs.brown.edu/"
-                target="_blank" rel="noreferrer">Brown Interactive 3D Vision & Learning Lab</a>
-                <div>
-                    <span className="research__subheading">Advisors: Stefanie Tellex, Srinath Sridhar</span>
-                </div>
-                <div className="research__description">
-                    <li>
-                        We present a data-driven model, UniTac, that leverages built-in joint torque sensors to achieve live whole-body touch sensing across various robot platforms, 
-                        eliminating the need for dedicated tactile sensors.
-                    </li>
-                    <li>
-                        UniTac demonstrates generalizability across multiple robot instances with the same type, allowing a wider community to use it as an off-the-shelf interface directly.
-                    </li>
-                    <li>
-                        We demonstrate potential applications in touch-based human-robot interaction, including scenarios such as bio-inspired quadruped choreography.
-                    </li>
-                </div>
-                <img src={UniTacImg} alt="" className="research__img" />
-            </div> 
-            <div className="research__box">
-                <div className="research__topic">
-                    <a className="research__title" href="https://ivl.cs.brown.edu/research/gigahands.html"> (CVPR 2025) GigaHands: A Massive Annotated Dataset of Bimanual Hand Activities </a>
-                    <span className="research__date">Jan 2024 - Nov 2024</span>
-                    <div className="research__social">
-                        <a href="https://www.arxiv.org/pdf/2412.04244" 
-                        className="research__social-icon" rel="noreferrer" target="_blank">
-                        <i class="uil uil-file-alt"></i>
-                        </a>
+                    <div>
+                        <span className="research__subheading">
+                            <strong>Wanjia Fu*</strong>,
+                            <a href="https://lhy.xyz/"> Hongyu Li*</a>,
+                            <a href="https://ivyyyy24381.github.io/home"> Ivy X. He</a>,
+                            <a href="https://cs.brown.edu/people/stellex/"> Stefanie Tellex</a>,
+                            <a href="https://cs.brown.edu/people/ssrinath/"> Srinath Sridhar</a>
+                        </span>
                     </div>
-                </div>
-                <a className="research__subtitle" href="https://ivl.cs.brown.edu/"
-                target="_blank" rel="noreferrer">Brown Interactive 3D Vision & Learning Lab</a>
-                {/* <div>
-                    <span className="research__subheading">Rao Fu*, Dingxi Zhang*, Alex Jiang, Wanjia Fu, Austin Funk, Daniel Ritchie, Srinath Sridhar</span>
-                </div> */}
-                <div>
-                    <span className="research__subheading">Advisors: Srinath Sridhar, Daniel Ritchie</span>
-                </div>
-                <div className="research__description">
-                    <li>
-                        Synchronized the 30 FPS frames in the RGB videos across 51 cameras in the Brown Interactive Capture Systems for accurate 2D and 3D key points and MANO model fitting. 
-                    </li>
-                    <li>
-                        Generated and accumulated contact heatmap on the palms and backs of the left and right hand across bimanual hand-object and hand-hand activities of diverse scenes, such as boxing, massage, frying, and tea drinking. 
-                    </li>
-                    <li>
-                        Collected data for the GigaHands dataset, mostly focusing on makeup, cleaning, and clothes folding scenes, which consist of more than forty actions with text annotations; and scanned the objects for 3D reconstruction. 
-                    </li>
-                    <li>
-                        Tried to implement 3D Dynamic Gaussian Splatting on the multi-view camera data of scenes. 
-                    </li>
-                </div>
-                <img src={HandsImg} alt="" className="research__img" />
-            </div> 
-            <div className="research__box">
-                <div className="research__topic">
-                    <h3 className="research__title">Vibration Haptics: Hand contact detection and localization with IMU</h3>
-                    <span className="research__date">Jan 2024 - Sep 2024</span>
-                </div>
-                <a className="research__subtitle" href="https://ivl.cs.brown.edu/"
-                target="_blank" rel="noreferrer">Brown Interactive 3D Vision & Learning Lab</a>
-                <div>
-                    <span className="research__subheading">Advisors: Srinath Sridhar, Krishna Murthy Jatavallabhula</span>
-                </div>
-                <div className="research__description">
-                    <li>
-                        {/* Training a model to detect hand-object contact and to localize the contact onto fingers with acceleration and gyroscope data from IMU sensors.  */}
-                        Designed portable wrist hardware device containing with Inertial Measurement Unit (IMU), using which to collect hand object contact sensor and visual data
-                    </li>
-                    <li>
-                        {/* Developed and 3D printed brace watch-like model to incorporate IMU and collect data. */}
-                        Preprocessed RGB camera data with MANO fitting pipeline to obtain pose estimation results and contact heatmap, 
-                        built a neural network to improve hand-object contact detection and localization 
-                    </li>
-                    <li>
-                        {/* Collected data using Brown Interactive Camera System (BRICS) and labeled data by incorporating the MANO fitting pipeline.  */}
-                        Integrated IMU sensor and its data visualization into Brown Interaction Capture System (BRICS), participated in the hardware assembling and software calibration for the room-sized capture studio with professor Dr. Srinath Sridhar
-                    </li>
-                </div>
-                <img src={ResearchImg} alt="" className="research__img" />
-            </div>
-            <div className="research__box">
-                <div className="research__topic">
-                    <h3 className="research__title">Shaped-Based Skill Transfer by Learning Policy on Object Parts</h3>
-                    <span className="research__date" id="irl">2023-2024</span>
-                </div>
-                <a className="research__subtitle" href="http://irl.cs.brown.edu/"
-                target="_blank" rel="noreferrer">Brown Intelligent Robotics Lab</a>
-                <div>
-                    <span className="research__subheading">Advisor: George Konidaris</span>
-                </div>
-                <div className="research__description">
-                    <li>
-                    Learned latent representations of object parts for mugs and spatulas to carry out pouring and scooping tasks, and tried to learn a 
-                    robust skill for all shapes of the same object category, with advisor Dr. George Konidaris
-                    </li>
-                    <li>
-                        Incorporated Segment-Anything model to segment images of mugs into cup and handle and back projected the segmentation into point clouds. 
-                    </li>
-                    <li>
-                        Developed a policy with lower costs than when learning on the whole object instead of object parts
-                    </li>
-                    <li>
-                        Worked on ROS and reinforcement learning, Boston Dynamics Spot robots, and KUKA robotic arms with radar cameras
-                    </li>
+                    <div className="research__subtitle">
+                        <p><i>Preprint</i></p>
+                        <p>Presented at RSS 2025 <a href="https://hrcm-workshop.github.io/2025/">HRCM workshop</a></p>
+                    </div>
                     
+                    <div className="research__description">
+                        <p>
+                            We present a data-driven model, UniTac, that leverages built-in joint torque sensors to achieve live whole-body touch sensing across various robot platforms, 
+                            eliminating the need for dedicated tactile sensors. 
+                        </p>
+                    </div>
+                    <div className="research__links">
+                        <a href="https://arxiv.org/abs/2507.07980"class="btn btn-sm z-depth-0" role="button" target="_blank" rel="noreferrer">arXiv</a>
+                        <a href="https://ivl.cs.brown.edu/research/unitac"class="btn btn-sm z-depth-0" role="button" target="_blank" rel="noreferrer">HTML</a>
+                    </div>
+
                 </div>
-                <img src={IRLImg} alt="" className="research__img" />
-            </div>
+            </div> 
+            <div className="research__box">
+                <div className="research__teaser">
+                    <img src={HandsImg} alt="" className="research__img" />
+                </div>
+                <div className="research__paragraph">
+                    <div className="research__topic">
+                        <a className="research__title" href="https://ivl.cs.brown.edu/research/gigahands.html"> GigaHands: A Massive Annotated Dataset of Bimanual Hand Activities </a>
+                        {/* <div className="research__social">
+                            <a href="https://www.arxiv.org/pdf/2412.04244" 
+                            className="research__social-icon" rel="noreferrer" target="_blank">
+                            <i class="uil uil-file-alt"></i>
+                            </a>
+                        </div> */}
+                    </div>
+                    <div>
+                        <span className="research__subheading">
+                            <a href="https://freddierao.github.io/">Rao Fu*</a>,
+                            <a href="https://kristen-z.github.io/"> Dingxi Zhang*</a>
+                            <a href="https://www.alex-jiang.com/about/"> Alex Jiang</a>,
+                            <strong> Wanjia Fu</strong>,
+                            <a href="https://austin-funk.github.io/"> Austin Funk</a>,
+                            <a href="https://dritchie.github.io/"> Daniel Ritchie</a>,
+                            <a href="https://cs.brown.edu/people/ssrinath/"> Srinath Sridhar</a>
+                        </span>
+                    </div>
+                    <p className="research__subtitle"><i>CVPR, 2025 (Highlight)</i></p>
+                    <div className="research__description">
+                        <p>
+                            We introduce GigaHands, a massive annotated dataset capturing 34 hours of bimanual hand activities from 56 subjects and 417 objects, 
+                            totaling 14k motion clips derived from 183 million frames paired with 84k text annotations. 
+                        </p>
+                    </div>
+                    <div className="research__links">
+                        <a href="https://www.arxiv.org/abs/2412.04244"class="btn btn-sm z-depth-0" role="button" target="_blank" rel="noreferrer">arXiv</a>
+                        <a href="https://ivl.cs.brown.edu/research/gigahands.html"class="btn btn-sm z-depth-0" role="button" target="_blank" rel="noreferrer">HTML</a>
+                        <a href="https://github.com/Kristen-Z/GigaHands"class="btn btn-sm z-depth-0" role="button" target="_blank" rel="noreferrer">Dataset</a>
+                    </div>
+                </div>
+            </div> 
+            
         </div>
     )
 }
