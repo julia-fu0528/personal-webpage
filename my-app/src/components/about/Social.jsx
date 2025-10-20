720dee1bdc8929f1970773d981a4931d7cb0021e
Julia Fu <wanjia_fu@brown.edu>
2025-10-20 16:02:02 -0400

    google scholar

--- my-app/src/components/about/Social.jsx
+++ my-app/src/components/about/Social.jsx
@@ -3,6 +3,10 @@ import React from "react";
 const Social = () => {
     return (
         <div className="about__social">
+            <a href="https://scholar.google.com/citations?user=Y7gW52cAAAAJ" 
+            className="about__social-icon" rel="noreferrer" target="_blank">
+            <i class="ai ai-google-scholar"></i>
+            </a>
             <a href="https://github.com/julia-fu0528" 
             className="about__social-icon" rel="noreferrer" target="_blank">
             <i class="uil uil-github"></i>
@@ -11,6 +15,7 @@ const Social = () => {
             className="about__social-icon" rel="noreferrer" target="_blank">
             <i class="uil uil-linkedin"></i>
             </a>
+            
         </div>
     );
    }
